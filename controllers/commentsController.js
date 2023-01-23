4f00a8bd31de687a57024b0e9af7cf02ba8bf190
Gary N <gary.newton@ymail.com>
2023-01-22 21:15:51 -0500

    Misc.

--- controllers/commentsController.js
+++ controllers/commentsController.js
@@ -7,7 +7,7 @@ const Comment = require('../models/Comment.js');
 
 // Remember INDUCES
 
-// Index
+// Index - Get all comments
 router.get('/', (req, res) => {
     Comment.find({}, (err, foundComments) => {
         res.json(foundComments);
@@ -16,21 +16,21 @@ router.get('/', (req, res) => {
 
 // New - Will be handled by React application
 
-// Delete
+// Delete - Delete a comment by commentId
 router.delete('/:id', (req, res) => {
     Comment.findByIdAndRemove(req.params.id, (err, deletedComment) => {
         res.json(deletedComment);
     });
 });
 
-// Update
+// Update - Update a comment by commentId
 router.put('/:id', (req, res) => {
     Comment.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedComment) => {
         res.json(updatedComment);
     });
 });
 
-// Create
+// Create - Create a new comment
 
 router.post('/', (req, res) => {
     Comment.create(req.body, (err, createdComment) => {
@@ -44,11 +44,47 @@ router.post('/', (req, res) => {
 
 // Edit - Will be handled by React application
 
-// Show
+// Show - Get a comment by commentId
 router.get('/:id', (req, res) => {
     Comment.findById(req.params.id, (err, foundComment) => {
         res.json(foundComment);
     });
 });
+
+// ***UPDATE***
+
+// Get  all comments by userId getLLCommentsByUser
+router.get('/user/:userId', (req, res) => {
+    Comment.find({ user: req.params.userId }, (err, foundComments) => {
+        res.json(foundComments);
+    });
+});
+
+// Get all comments by messageId getAllCommentsByMessage
+router.get('/message/:messageId', (req, res) => {
+    Comment.find({ message: req.params.messageId }, (err, foundComments) => {
+        res.json(foundComments);
+    });
+});
+
+// Create the endpoint in the backend for getAllCommentsByUser with mongoose
+// router.get('/comments/user/:userId', async (req, res) => {
+//     try {
+//         const foundComments = await Comment.find({ user: req.params.userId });
+//         res.json(foundComments);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
+// router.get('/comments/user/:userId', async (req, res) => {
+//     try {
+//         const foundComments = await Comment.find({ user: req.params.userId });
+//         res.json(foundComments);
+//     } catch (error) {
+//         res.status(500).json({ error: error.message });
+//     }
+// });
+
 
 module.exports = router;
